2e2c383420fba9faad4121191ccaa12f20ebf083
Jake <jakeroggenbuck2@gmail.com>
2022-09-19 01:33:31 -0700

    Add more

---
+++ README.md
@@ -0,0 +1,12 @@
+# Day
+
+## Backend
+
+## Frontend
+
+#### Color Scheme
+black: #000000;
+oxford-blue: #14213d;
+orange-web: #fca311;
+platinum: #e5e5e5;
+white: #ffffff;
--- backend/main.go
+++ backend/main.go
@@ -1,19 +1,28 @@
 package main
 
 import (
-  "net/http"
-
-  "github.com/gin-gonic/gin"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 func main() {
 	router := gin.Default()
-	
+	router.SetTrustedProxies([]string{"localhost"})
+
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Welcome to Day.",
+		})
+	})
+
 	router.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
-    		"message": "pong",
+			"message": c,
 		})
-  	})
+	})
 
-  	router.Run(":8080")
+	port := ":8080"
+	fmt.Println("http://localhost" + port)
+	router.Run(port)
 }
